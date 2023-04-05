73f61772ee88c47c827ed6466b9c1e0e7df4b610
Randira <204060028.mhs@stmikjabar.ac.id>
2023-04-05 23:22:59 +0700

    add getElementsByClassName()

--- script.js
+++ script.js
@@ -14,3 +14,7 @@ for (let i = 0; i < p.length; i++) {
 // mengubah object menjadi element agar bisa di beri style
 const h1 = document.getElementsByTagName('h1')[0];
 h1.style.fontSize = '50px';
+
+// document.getElementsByClassName()
+const p1 = document.getElementsByClassName('p1')[0];
+p1.innerHTML = 'Join us Instagram';
