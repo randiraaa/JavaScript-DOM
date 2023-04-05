2b384e8784dac12d52bce5badd832892c34765c9
Randira <204060028.mhs@stmikjabar.ac.id>
2023-04-05 23:22:10 +0700

    add getElementsByTagName()

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const judul = document.getElementById('judul');
 judul.style.color = 'black';
 judul.style.backgroundColor = 'yellow';
 judul.innerHTML = 'My name is Randi.';
+
+// document.getElementsByTagName()
+const p = document.getElementsByTagName('p');
+
+for (let i = 0; i < p.length; i++) {
+    p[i].style.backgroundColor = 'brown';   
+}
+
+// mengubah object menjadi element agar bisa di beri style
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '50px';
