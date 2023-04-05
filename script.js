e2a2328113e2e4410c4d6a133c38d00ae0a89f7d
Randira <204060028.mhs@stmikjabar.ac.id>
2023-04-05 23:20:44 +0700

    add getElemenById()

---
+++ script.js
@@ -0,0 +1,5 @@
+// document.getElemenById()
+const judul = document.getElementById('judul');
+judul.style.color = 'black';
+judul.style.backgroundColor = 'yellow';
+judul.innerHTML = 'My name is Randi.';
