cbfbb0d8f6a0584d2c394d04ee117573bb202775
Randira <204060028.mhs@stmikjabar.ac.id>
2023-04-05 23:25:43 +0700

    document.querySelectorAll()

--- script.js
+++ script.js
@@ -23,3 +23,8 @@ p1.innerHTML = 'Join us Instagram';
 const p4 = document.querySelector('#b p');
 p4.style.color = 'white';
 p4.style.fontSize = '20px';
+
+// document.querySelectorAll()
+const allSelectP = document.querySelectorAll('p')[2];
+allSelectP.style.backgroundColor = 'black';
+
