9c6b0e13e8189e84534d66493e50bb4e514b3c79
Randira <204060028.mhs@stmikjabar.ac.id>
2023-04-06 14:43:14 +0700

    selecting DOM fixed

---
+++ script-selecting.js
@@ -0,0 +1,34 @@
+// Selection Elements
+
+// document.getElemenById()
+const judul = document.getElementById('judul');
+judul.style.color = 'black';
+judul.style.backgroundColor = 'yellow';
+judul.innerHTML = 'My name is Randi.';
+
+// document.getElementsByTagName()
+const p = document.getElementsByTagName('p');
+
+for (let i = 0; i < p.length; i++) {
+    p[i].style.backgroundColor = 'brown';   
+}
+
+// mengubah object menjadi element agar bisa di beri style
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '50px';
+
+// document.getElementsByClassName()
+const p1 = document.getElementsByClassName('p1')[0];
+p1.innerHTML = 'Join us Instagram';
+
+// document.querySelector()
+const p4 = document.querySelector('#b p');
+p4.style.color = 'white';
+p4.style.fontSize = '20px';
+
+// document.querySelectorAll()
+const allSelectP = document.querySelectorAll('p')[2];
+allSelectP.style.backgroundColor = 'black';
+
+// document.getElementsByName()
+const btn = document.getElementsByName('btn');
